1fc4bd3b9ec8cf9854391d366f89f6436d5f88fe
Richard Schoen <richard@richardschoen.net>
2024-02-02 22:23:01 -0600

    Delete IbmiOdbcQueryToOutputFile_Run.bat

--- IbmiOdbcQueryToOutputFile_Run.bat
+++
@@ -1,7 +0,0 @@
-cd %~dp0
-rem powershell IbmiOdbcQueryToOutputFile.ps1 -ibmihost SYSi1 -ibmiuser RICHARD -ibmipass IJS2032 -sql 'select * from qiws.qcustcdt' -outputfile 'c:\rjstemp\qcustcdttxt'
-pwsh IbmiOdbcQueryToOutputFile.ps1 -ibmihost "sysi1" -ibmiuser "RICHARD" -ibmipass "IJS2032" -sql "select * ddfrom qiws.qcustcdt" -outputfile "c:\rjstemp\qcustcdt.txt"
-
-echo "Error level:" %errorlevel%
-
-pause
