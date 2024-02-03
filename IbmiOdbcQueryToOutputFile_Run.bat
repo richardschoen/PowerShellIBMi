6722e08d85993d995ec8e6a8dd6a92051a41ad88
Richard Schoen <richard@richardschoen.net>
2024-02-02 22:17:57 -0600

    Create IbmiOdbcQueryToOutputFile_Run.bat

---
+++ IbmiOdbcQueryToOutputFile_Run.bat
@@ -0,0 +1,7 @@
+cd %~dp0
+rem powershell PSIbmiOdbcQueryToOutputFile.ps1 -ibmihost SYSi1 -ibmiuser RICHARD -ibmipass IJS2032 -sql 'select * from qiws.qcustcdt' -outputfile 'c:\rjstemp\qcustcdttxt'
+pwsh IbmiOdbcQueryToOutputFile.ps1 -ibmihost "sysi1" -ibmiuser "RICHARD" -ibmipass "IJS2032" -sql "select * ddfrom qiws.qcustcdt" -outputfile "c:\rjstemp\qcustcdt.txt"
+
+echo "Error level:" %errorlevel%
+
+pause
